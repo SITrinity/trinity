8a5d7b873d2c4d265d304071e9faa36a96a2318e
edwin <emarquez26@misena.edu.co>
2015-11-08 23:27:26 -0500

    trinity proyect third deploy

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -6,5 +6,17 @@ class MyApp < Sinatra::Application
   set :public_folder, 'public'
   get '/' do
     erb :'index.html'
-  end 
+  end
+  get '/trinity.html' do
+    erb :'trinity.html'
+  end
+  get '/enlace.html' do
+    erb :'enlace.html'
+  end
+  get '/team.html' do
+    erb :'team.html'
+  end
+  get '/designs.html' do
+    erb :'designs.html'
+  end
 end
