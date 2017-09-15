d64670c9ee1c10a745e6c80206f5cb40e42f644a
Mac002 <mac002@iMac-de-Mac002.local>
2017-09-15 14:25:48 -0500

    adding a 360 grades video taking into UA

--- public/js/float_button.js
+++ public/js/float_button.js
@@ -4,3 +4,13 @@ $('.btn_principal').hover(function(){
 $('.btn_container').mouseleave(function(){
   $('.btn_float').removeClass('animacionVer');
 })
+function ocultar_div(){
+	$("#all-body").fadeOut(600);
+	$(".video_360").remove();
+}
+$(document).keyup(function(event){
+        if(event.which==27)
+        {
+            ocultar_div();
+        }
+    });
