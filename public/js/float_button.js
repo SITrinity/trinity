40f8cf1341e7cdc2a189831fbffd393fccee2795
edwin <edwin.marquez261@gmail.com>
2017-09-20 14:39:09 -0500

    adding proyects

--- public/js/float_button.js
+++ public/js/float_button.js
@@ -8,9 +8,13 @@ function ocultar_div(){
 	$("#all-body").fadeOut(600);
 	$(".video_360").remove();
 }
+function mostrar_div() {
+    $("#all-body").fadeIn(600);
+    $("#all-body").append("<iframe class=\"video_360\" src=\"https://www.youtube.com/embed/xD2T3B01bRo?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1&fs=0\" frameborder=\"0\"</iframe>").fadeIn(600);
+}
 $(document).keyup(function(event){
-        if(event.which==27)
-        {
-            ocultar_div();
-        }
-    });
+    if(event.which==27) 
+    {
+        ocultar_div();
+    }
+});
